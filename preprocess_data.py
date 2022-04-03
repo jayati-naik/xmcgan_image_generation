1a0528a010443d6451e915080114f658493119de
Hitesh Pindikanti <pindikan@discovery2.hpc.usc.edu>
2022-04-02 19:28:23 -0700

    corrected preprocess.py file

--- preprocess_data.py
+++ preprocess_data.py
@@ -15,8 +15,7 @@
 from typing import List
 
 # import bert
-# from bert import tokenization
-from bert.tokenization import bert_tokenization as tokenization
+from bert import tokenization
 
 import numpy as np
 import tensorflow as tf
@@ -99,21 +98,21 @@ def serialize_example(ex):
 if __name__ == '__main__':
     # Preprocess train and val data.
     for process_split in ['train', 'validation']:
-        tfds_splits = ['train[:100]']
+        tfds_splits = ['train[:5000]']
         # COCO-2014 consists of 40k examples from these three splits.
         if process_split == 'validation':
-            tfds_splits = ['restval[:20]', 'test[20:40]', 'val[40:60]']
+            tfds_splits = ['restval[:500]', 'test[500:1000]', 'val[1000:1500]']
 
         output_path = f'data/coco2014_{process_split}.tfrecord'
         with tf.io.TFRecordWriter(output_path) as file_writer:
             for tfds_split in tfds_splits:
-                ds = tfds.load('coco_captions', split=tfds_split)
+                ds = tfds.load('coco_captions', split=tfds_split, data_dir='/scratch/pindikan/tensorflow_datasets')
                 for features in tqdm(ds, position=0):
                     file_writer.write(serialize_example(features))
 
         # Shard dataset.
         raw_dataset = tf.data.TFRecordDataset(output_path)
-        num_shards = 1
+        num_shards = 10
         for i in tqdm(range(num_shards)):
             writer = tf.data.experimental.TFRecordWriter(
                 f'{output_path}-{i}-of-{num_shards}')
