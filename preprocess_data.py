34f40580861c8a29c7a3ef5396b447db44b2c4a4
Jayati Naik <naik.jayati89@gmail.com>
2022-04-21 12:15:38 -0700

    Update preprocess_data.py

--- preprocess_data.py
+++ preprocess_data.py
@@ -94,37 +94,35 @@ def serialize_example(ex):
     tf_ex = tf.train.Example(features=tf.train.Features(feature=context_features))
     return tf_ex.SerializeToString()
 
-
 if __name__ == '__main__':
-    # Preprocess train and val data.
-    
-    coco_mini_dataset = set(line.strip() for line in open('/home1/jnaik/CSCI566-Project/code_base/xmcgan_image_generation/data/minicoco_train_fnames_2014.txt'))
+  # Preprocess train and val data.
+  coco_mini_dataset = set(line.strip() for line in open('/ifs/loni/faculty/thompson/four_d/jnaik/xmcgan_image_generation/data/minicoco_train_fnames_2014.txt'))
+  print(coco_mini_dataset)
+  for process_split in ['train', 'validation']:
+      tfds_splits = ['train']
+      # COCO-2014 consists of 40k examples from these three splits.
+      if process_split == 'validation':
+          tfds_splits = ['restval', 'test', 'val']
 
-    for process_split in ['train', 'validation']:
-        tfds_splits = ['train']
-        # COCO-2014 consists of 40k examples from these three splits.
-        if process_split == 'validation':
-            tfds_splits = ['restval', 'test', 'val']
+      output_path = f'data/coco2014_{process_split}.tfrecord'
+      with tf.io.TFRecordWriter(output_path) as file_writer:
+          for tfds_split in tfds_splits:
+              ds = tfds.load('coco_captions', split=tfds_split, data_dir='/ifs/loni/faculty/thompson/four_d/jnaik/cocodataset2014/data')
+              for features in tqdm(ds, position=0):
+                  filename = features['image/filename']
+                  filename = bytes.decode(filename.numpy()).split('.jpg')[0]
+                  if tfds_split == 'train':
+                      if filename in coco_mini_dataset:
+                         file_writer.write(serialize_example(features))
+                  else:
+                      file_writer.write(serialize_example(features))
 
-        output_path = f'data/coco2014_{process_split}.tfrecord'
-        with tf.io.TFRecordWriter(output_path) as file_writer:
-            for tfds_split in tfds_splits:
-                count = 0 
-                ds = tfds.load('coco_captions', split=tfds_split, data_dir='/scratch1/jnaik/tensorflow_datasets')
-                for features in tqdm(ds, position=0):
-                    filename = features['image/filename']
-                    print(filename)
-                    if tfds_split != 'train' or filename in coco_mini_dataset:
-                        count += 1
-                        file_writer.write(serialize_example(features))
-                print(count)
-
-        # Shard dataset.
-        raw_dataset = tf.data.TFRecordDataset(output_path)
-        num_shards = 10
-        for i in tqdm(range(num_shards)):
-            writer = tf.data.experimental.TFRecordWriter(
-                f'{output_path}-{i}-of-{num_shards}')
-            writer.write(raw_dataset.shard(num_shards, i))
-        # Remove unsharded dataset.
-        tf.io.gfile.remove(output_path)
+      # Shard dataset.
+      raw_dataset = tf.data.TFRecordDataset(output_path)
+      num_shards = 100
+      for i in range(num_shards):
+        writer = tf.data.experimental.TFRecordWriter(
+            f'{output_path}-{i}-of-{num_shards}')
+        writer.write(raw_dataset.shard(num_shards, i))
+      # Remove unsharded dataset.
+      tf.io.gfile.remove(output_path)
