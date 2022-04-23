b4109281d4afb3232202a3b8efdd6e6dae7f1d28
Jayati Naik <naik.jayati89@gmail.com>
2022-04-23 15:10:21 -0700

    Update train.sh

--- train.sh
+++ train.sh
@@ -15,7 +15,7 @@
 #!/bin/bash
 CONFIG="xmcgan/configs/coco_xmc.py"
 EXP_NAME=$1
-WORKDIR="/home1/pindikan/CSCI566-Project/code_base/experiments/$EXP_NAME"  # CHANGEME
+WORKDIR="/home1/jnaik/CSCI566-Project/code_base/experiments/$EXP_NAME"  # CHANGEME
 
 CUDA_VISIBLE_DEVICES="1" python -m xmcgan.main \
   --config="$CONFIG" \
