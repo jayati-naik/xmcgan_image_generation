102aa020f13e94792838740dc452f8813cea8ad1
jayati-naik <jnaik@usc.edu>
2022-05-01 04:25:00 -0700

    iter fixed

--- xmcgan/utils/eval_metrics.py
+++ xmcgan/utils/eval_metrics.py
@@ -199,7 +199,7 @@ class EvalMetric:
     for iter in range(4):
       for step in range(n_iter):
         inputs = jax.tree_map(np.asarray, next(self.ds))  # pytype: disable=wrong-arg-types
-        inputs['iter'] = iter
+        inputs['iter'] = list(iter)
         step_sample_batch_rng = jax.random.fold_in(rng, step)
         step_sample_batch_rngs = jax.random.split(step_sample_batch_rng,
                                                   jax.local_device_count())
